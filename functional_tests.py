76aa82eaf01c63c3b509414fa19c7e2b50781eb3
aclcheuk <aclcheuk@outlook.com>
2026-05-04 22:14:40 +0100

    Spec out FT using comments and unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,36 @@
 from selenium import webdriver
+import unittest
+# Functional Test == End-to-End Test == Acceptance Test
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert "Congratulations!" in browser.title
-print("OK")
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_start_a_todo_list(self):
+        # Anthony wants to access a new To-Do App
+        # He visits the homepage:
+        self.browser.get("http://localhost:8000")
+
+        # He notes the title and header mentions To-Do lists
+        self.assertIn("To-Do", self.browser.title)
+
+        # He is invited to enter a to-do item straight away
+        self.fail("Remember to finish the test!")
+
+        # He types "Fold away laundry"
+
+        # When he hits Enter, the page updates and now lists:
+        # "1: Fold away laundry" as an item in the to-do list
+
+        # A tet box remains to input more items
+        # He types "Hang painting"
+
+        # The page updates again and both items are on the list
+
+        # Satisfied, he goes back to sleep
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ notes.md
@@ -0,0 +1,11 @@
+# Test-Driven Development
+
+## Starting a Project:
+1) Set up directory and venv
+2) make `functional_tests.py` file with an expected fail
+3) 
+
+## Using unittest
+Current trend is towards using pytest but here we use unittest
+- `setUp(self)` and `tearDown(self)` to start and stop test
+- `test_descriptive_name`
